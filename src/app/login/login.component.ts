3530adf173fcf6961cd7ff656103ba8a747cdcd2
Emmanuel_Sewah <sewah2012@gmail.com>
2021-12-28 11:11:11 +0100

    implement basic routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,26 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+ 
+  constructor(private router:Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {  }
 
   email: FormControl = new FormControl('');
   mdp:FormControl = new FormControl('')
+
+
+
+  onSubmit(){
+    if(this.email.value.trim() == 'admin' && this.mdp.value.trim()=='admin123'){
+      this.router.navigate(['affectation'])
+    } else{
+      alert('email ou mdp not correct!')
+    }
+
+    
+
+    // console.log('email: ',this.email.value, 'password: ',this.mdp.value)
+  }
   
 }
