9561a4112178fb08405b14de34eab7ae71d29972
Emmanuel_Sewah <sewah2012@gmail.com>
2021-12-28 04:22:15 +0100

    push to github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent},
   {path:'affectation',component:HomeComponent}
 ];
 
